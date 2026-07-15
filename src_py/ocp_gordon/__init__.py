d923a1ebd1d2bb2a526af29afd302bece953ff4a
Fan Gong <gongfan99@hotmail.com>
2026-07-15 19:27:15 -0400

    Include package version in __init__.py
    Added version retrieval for the ocp-gordon package.

--- src_py/ocp_gordon/__init__.py
+++ src_py/ocp_gordon/__init__.py
@@ -4,7 +4,12 @@ Gordon curve interpolation in Python for CadQuery's OCP.
 This module provides Python implementation of the Gordon curve interpolation
 algorithm originally designed for OpenCASCADE, compatible with CadQuery's OCP.
 """
-
+from importlib.metadata import version, PackageNotFoundError
 from .internal.interpolate_curve_network import interpolate_curve_network, interpolate_curve_network_debug
 
-__all__ = ['interpolate_curve_network', 'interpolate_curve_network_debug']
+try:
+    __version__ = version("ocp-gordon")
+except PackageNotFoundError:
+    __version__ = "unknown version"
+
+__all__ = ['interpolate_curve_network', 'interpolate_curve_network_debug', '__version__']
